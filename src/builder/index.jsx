76b1432d6a6f3190fdb203b6bb05b82e84fef7c9
heracles <heracles.siiz@gmail.com>
2023-01-07 03:54:53 +0900

    add plus icon in cell and add form

--- src/builder/index.jsx
+++ src/builder/index.jsx
@@ -15,7 +15,7 @@ import './grapes.css';
 import './index.scss';
 import 'semantic-ui-css/semantic.min.css';
 
-const svgNameList = ['column', '2columns', '3columns', '2col37', 'text', 'link', 'image', 'video', 'map', 'linkblock', 'quote', 'textsection', 'form', 'input', 'textarea', 'select', 'button', 'label', 'checkbox', 'radio', 'navbar', 'countdown'
+const svgNameList = ['column', '2columns', '3columns', '2col37', 'text', 'link', 'image', 'video', 'map', 'linkblock', 'quote', 'textsection', 'form','form', 'input', 'textarea', 'select', 'button', 'label', 'checkbox', 'radio', 'navbar', 'countdown'
 ];
 
 const panelList = [];
@@ -46,6 +46,9 @@ export default function Buidler(props) {
                 basic, plugin, forms, navbar, countdown, pgexport
             ],
             pluginsOpts: {
+                [forms]:{
+                    blocks:[ 'input', 'textarea', 'select', 'button', 'label', 'checkbox', 'radio']
+                },
                 [pgexport]: {
                     addExportBtn: true,
                     btnLabel: 'export',
@@ -192,13 +195,297 @@ export default function Buidler(props) {
         });
 
         const blockManager = editor.Blocks;
+        
+        blockManager.add('lead_generation', {
+            label: 'Lead Generation',
+            content: 
+                `<form method='get' action='/'>
+                    <input type='text' placeholder='Name' name='form_name' class='form_name'>
+                    <input type='email' placeholder='Email' name='form_email' class='form_email'>
+                    <input type='submit' value='Sign Up' name='form_sign' class='form_sign'>
+                </form>
+                <style>
+                    input {
+                        width:100%;
+                        height:35px;
+                        margin-bottom:0.3em;
+                    }
+                    .form_name,.form_email{
+                        backgrond-color:#ccc;
+                    }
+                    .form_sign{
+                        background-color:Aqua;
+                    }
+                </style>
+                `
+          }, { at: 12 });
+
+        blockManager.add('contact_form', {
+        label: 'Contact Form',
+        content: 
+            `<form method='get' action='/'>
+                <input type='text' placeholder='Name' name='form_name' class='form_name'>
+                <input type='email' placeholder='Email' name='form_email' class='form_email'>
+                <input type='textarea' placeholder='Message' name='form_message' class='form_message'>
+                <input type='submit' value='Send Message' name='form_send' class='form_send'>
+            </form>
+            <style>
+                input {
+                    width:100%;
+                    height:35px;
+                    margin-bottom:0.3em;
+                }
+                input[type='textarea'] {
+                    height:120px;
+                }
+                .form_name,.form_email{
+                    backgrond-color:#ccc;
+                }
+                .form_send{
+                    background-color:Aqua;
+                }
+            </style>
+            `,
+        }, { at: 13 });
+
         let blocks = blockManager.getAll();
 
         blocks.map((block, index) => {
             block.attributes.media = '<img src = "buildericons/' + svgNameList[index] + '.svg">';
+            console.log(block.attributes.content);
+            
+            switch(block.attributes.label){
+                case '1 Column':                    
+                    block.attributes.content =
+                    `<div class='gjs-row' data-text='&#43;' data-gjs-droppable='.gjs-cell' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":0,"bl":0,"br":0,"minDim":1}' data-gjs-name='Row'>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                    </div>
+                    <style>  
+                        .gjs-row {
+                            display: table;
+                            padding: 10px;
+                            width: 100%;
+                        }
+                        .gjs-cell {
+                            width: 8%;
+                            display: table-cell;
+                            height: 75px;
+                        }
+                        
+                        @media (max-width: 768px) {
+                            .gjs-cell, .gjs-cell30, .gjs-cell70 {
+                                width: 100%;
+                                display: block;
+                            }
+                        }
+                                            
+                        .gjs-row:empty:not(:focus)  {
+                            background-color:#555;
+                            border:1px solid #ddd;
+                        }
+                        .gjs-cell:empty:not(:focus)  {
+                            background-color:#888;
+                            border:1px solid #eee;
+                        }
+                        .gjs-cell:empty:not(:focus)::before  {
+                            content: attr(data-text);
+                            position: relative;
+                            background-color: #ccc;
+                            padding: 0.3em 0.5em;
+                            border-radius: 50%;
+                            top: calc(50% - 0.5em);
+                            left: calc(50% - 0.5em);
+                        }
+                        .gjs-row:empty:not(:focus)::before {
+                            content: attr(data-text);
+                            position: relative;
+                            background-color: #ccc;
+                            padding: 0.3em 0.5em;
+                            border-radius: 50%;
+                            top: calc(50% - 0.5em);
+                            left: calc(50% - 0.5em);
+                        }
+                    </style>`;
+                    break;
+                case '2 Columns':
+                    block.attributes.content = 
+                        `<div  class='gjs-row' data-text='&#43;' data-gjs-droppable='.gjs-cell' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":0,"bl":0,"br":0,"minDim":1}' data-gjs-name='Row'>
+                            <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                            <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                        </div>
+                        <style>
+                        .gjs-row {
+                            display: table;
+                            padding: 10px;
+                            width: 100%;
+                        }
+                        .gjs-cell {
+                            width: 8%;
+                            display: table-cell;
+                            height: 75px;
+                        }
+                        @media (max-width: 768px) {
+                            .gjs-cell, .gjs-cell30, .gjs-cell70 {
+                                width: 100%;
+                                display: block;
+                            }
+                        }
+                                            
+                        .gjs-row:empty:not(:focus)  {
+                            background-color:#555;
+                            border:1px solid #ddd;
+                        }
+                        .gjs-cell:empty:not(:focus)  {
+                            background-color:#888;
+                            border:1px solid #eee;
+                        }
+                        .gjs-cell:empty:not(:focus)::before  {
+                            content: attr(data-text);
+                            position: relative;
+                            background-color: #ccc;
+                            padding: 0.3em 0.5em;
+                            border-radius: 50%;
+                            top: calc(50% - 0.5em);
+                            left: calc(50% - 0.5em);
+                        }
+                        .gjs-row:empty:not(:focus)::before {
+                            content: attr(data-text);
+                            position: relative;
+                            background-color: #ccc;
+                            padding: 0.3em 0.5em;
+                            border-radius: 50%;
+                            top: calc(50% - 0.5em);
+                            left: calc(50% - 0.5em);
+                        }
+                        </style>`;
+                    break;
+                case '3 Columns':
+                    block.attributes.content = 
+                    `<div  class='gjs-row' data-text='&#43;' data-gjs-droppable='.gjs-cell' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":0,"bl":0,"br":0,"minDim":1}' data-gjs-name='Row'>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell'></div>
+                    </div>
+                    <style>
+                    .gjs-row {
+                        display: table;
+                        padding: 10px;
+                        width: 100%;
+                    }
+                    .gjs-cell {
+                        width: 8%;
+                        display: table-cell;
+                        height: 75px;
+                    }
+                    @media (max-width: 768px) {
+                        .gjs-cell, .gjs-cell30, .gjs-cell70 {
+                            width: 100%;
+                            display: block;
+                        }
+                    }
+                                            
+                    .gjs-row:empty:not(:focus)  {
+                        background-color:#555;
+                        border:1px solid #ddd;
+                    }
+                    .gjs-cell:empty:not(:focus)  {
+                        background-color:#888;
+                        border:1px solid #eee;
+                    }
+                    .gjs-cell:empty:not(:focus)::before  {
+                        content: attr(data-text);
+                        position: relative;
+                        background-color: #ccc;
+                        padding: 0.3em 0.5em;
+                        border-radius: 50%;
+                        top: calc(50% - 0.5em);
+                        left: calc(50% - 0.5em);
+                    }
+                    .gjs-row:empty:not(:focus)::before {
+                        content: attr(data-text);
+                        position: relative;
+                        background-color: #ccc;
+                        padding: 0.3em 0.5em;
+                        border-radius: 50%;
+                        top: calc(50% - 0.5em);
+                        left: calc(50% - 0.5em);
+                    }
+                    </style>`;
+                    break;
+                case '2 Columns 3/7':
+                    block.attributes.content = 
+                    `<div  class='gjs-row' data-text='&#43;' data-gjs-droppable='.gjs-cell' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":0,"bl":0,"br":0,"minDim":1}' data-gjs-name='Row'>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell' style='width: 30%;'></div>
+                        <div  class='gjs-cell' data-text='&#43;' data-gjs-draggable='.gjs-row' data-gjs-resizable='{"tl":0,"tc":0,"tr":0,"cl":0,"cr":1,"bl":0,"br":0,"minDim":1,"bc":0,"currentUnit":1,"step":0.2}' data-gjs-name='Cell' style='width: 70%;'></div>
+                    </div>
+                    <style>
+                    .gjs-row {
+                        display: table;
+                        padding: 10px;
+                        width: 100%;
+                    }
+                    .gjs-cell {
+                        width: 8%;
+                        display: table-cell;
+                        height: 75px;
+                    }
+                    .gjs-cell30 {
+                        width: 30%;
+                    }
+                            
+                    .gjs-cell70 {
+                        width: 70%;
+                    }
+                    @media (max-width: 768px) {
+                        .gjs-cell, .gjs-cell30, .gjs-cell70 {
+                            width: 100%;
+                            display: block;
+                        }
+                    }
+                                            
+                    .gjs-row:empty:not(:focus)  {
+                        background-color:#555;
+                        border:1px solid #ddd;
+                    }
+                    .gjs-cell:empty:not(:focus)  {
+                        background-color:#888;
+                        border:1px solid #eee;
+                    }
+                    .gjs-cell:empty:not(:focus)::before  {
+                        content: attr(data-text);
+                        position: relative;
+                        background-color: #ccc;
+                        padding: 0.3em 0.5em;
+                        border-radius: 50%;
+                        top: calc(50% - 0.5em);
+                        left: calc(50% - 0.5em);
+                    }
+                    .gjs-row:empty:not(:focus)::before {
+                        content: attr(data-text);
+                        position: relative;
+                        background-color: #ccc;
+                        padding: 0.3em 0.5em;
+                        border-radius: 50%;
+                        top: calc(50% - 0.5em);
+                        left: calc(50% - 0.5em);
+                    }
+                    </style>`
+                    break;
+            }
             blocks[index] = block;
         });
         blockManager.render(blocks);
+
+        editor.on('component:selected', () => {
+            const selected = editor.getSelected();
+            console.log(selected.attributes,editor.Panels);
+            const openBlocksBtn = editor.Panels.getButton('views', 'open-blocks');
+            if (selected.attributes.name == "Row" || selected.attributes.name == "Cell") {
+                if(!openBlocksBtn || !openBlocksBtn.get('active')){
+                    openBlocksBtn && openBlocksBtn.set('active', 1);
+                }
+            }
+        });
 
         const undoManager = editor.UndoManager
         undoManager.start();
@@ -209,7 +496,7 @@ export default function Buidler(props) {
         editor.on('stop:preview', () => {
             setIndex(4);
         });
-
+        
         editor.load();
         setEditor(editor);
       }, []);
